8ce4bd2a23d0d18ff247ced2c8fa1971a0ffda5b
Sondre Wittek <sondrewittek@gmail.com>
2018-10-19 11:38:29 +0200

    Adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { UploadComponent } from './upload/upload.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    UploadComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule
